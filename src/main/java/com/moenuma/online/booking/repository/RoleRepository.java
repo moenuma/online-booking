17795032f2b207db01ac52ce1319e664913561a9
Moe N <moenumasawa@gmail.com>
2022-09-27 18:27:37 +0900

    Add user and role repository

--- src/main/java/com/moenuma/online/booking/repository/RoleRepository.java
+++ src/main/java/com/moenuma/online/booking/repository/RoleRepository.java
@@ -1,2 +1,13 @@
-package com.moenuma.online.booking.repository;public interface RoleRepository {
+package com.moenuma.online.booking.repository;
+
+import com.moenuma.online.booking.model.ERole;
+import com.moenuma.online.booking.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(ERole name);
 }
--- src/main/java/com/moenuma/online/booking/repository/UserRepository.java
+++ src/main/java/com/moenuma/online/booking/repository/UserRepository.java
@@ -1,2 +1,16 @@
-package com.moenuma.online.booking.repository;public interface UserRepository {
+package com.moenuma.online.booking.repository;
+
+import com.moenuma.online.booking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(final String username);
+
+    Boolean existsByUsername(final String username);
+
+    Boolean existsByEmail(final String email);
 }
